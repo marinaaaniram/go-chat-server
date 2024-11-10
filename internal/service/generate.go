88419da91caa5d991fd2cc1218609ba5e697a9f3
Marina Timofeeva <marina.timofeeva@emergingtravel.com>
2024-11-10 16:12:16 +0300

    Added api tests

--- internal/service/generate.go
+++ internal/service/generate.go
@@ -2,3 +2,4 @@ package service
 
 //go:generate sh -c "rm -rf mocks && mkdir -p mocks"
 //go:generate minimock -i ChatService -o ./mocks/ -s "_minimock.go"
+//go:generate minimock -i MessageService -o ./mocks/ -s "_minimock.go"
